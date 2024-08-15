e2fe93d5c2085626ba506e7c1c4c1af3120c18c4
shanks-D-fury <shashanks9190@gmail.com>
2024-08-15 22:59:15 +0530

    String reverse

--- StringChapter.java
+++ StringChapter.java
@@ -1,10 +1,19 @@
+
+import java.util.Scanner;
+
 public class StringChapter {
     
     public static void main(String[] args) {
-        int a=-10;
-        int b=-20;
-        int c=-30;
-        String x= a>c?(a>b?"a":"b"):(c>b?"c":"b");
-        System.out.println("Largest is "+x);
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the num : ");
+        char[] x=sc.next().toCharArray();
+        for(int i=x.length-1;i>=0;i--){
+            System.out.print(x[i]);
+        }
+        System.out.print("\nEnter the num : ");
+        String sx=sc.next();
+        StringBuilder sb=new StringBuilder(sx);
+        System.out.println(sb.reverse());
+        sc.close();
     }
 }
