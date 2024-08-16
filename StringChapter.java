4294ceb1aec05be826abdc3ee8b2521a9531a70a
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 10:40:38 +0530

    Factorial and factorial in range

--- StringChapter.java
+++ StringChapter.java
@@ -2,31 +2,29 @@
 import java.util.Scanner;
 
 public class StringChapter {
-    public static int fact(int n){
-        // //simple using the loop
-        // int factx=1;
-        // while(n>0){
-        //     factx=n*factx;
-        //     n--;
-        // }
-        // System.out.println(factx);
-
-        //now using the recursion 
-        if(n==0 || n==1){
-            return 1;
+    public static boolean isPrime(int n){
+        if(n==2){
+            return true;
         }
-        else{
-            return n*fact(n-1);
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printPrime(int n){
+        for(int i=2;i<=n;i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
         }
     }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the n : ");
         int n = sc.nextInt();
-        System.out.print("Enter the r : ");
-        int r = sc.nextInt();
-        double x=fact(n)/(fact(r)*fact(n-r));
-        System.out.println(x);
+        printPrime(n);
         sc.close();
     }
 }
