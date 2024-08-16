82a29785f0a044863dc71bb007272822d4335d19
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 16:38:16 +0530

    Pattern Left based Pyramid

--- StringChapter.java
+++ StringChapter.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 
 public class StringChapter {
     public static void pattern(int n){
-        for(int i=n;i>=0;i--){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the n : ");
-        int n = sc.nextInt();
-        pattern(n);
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the n : ");
+            int n = sc.nextInt();
+            pattern(n);
+        }
     }
 }
