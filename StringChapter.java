9f828109ebd2366e7651f5304fc69e9acbb7bf13
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 10:16:16 +0530

    Factorial and binomial factorial

--- StringChapter.java
+++ StringChapter.java
@@ -2,33 +2,31 @@
 import java.util.Scanner;
 
 public class StringChapter {
-    
+    public static int fact(int n){
+        // //simple using the loop
+        // int factx=1;
+        // while(n>0){
+        //     factx=n*factx;
+        //     n--;
+        // }
+        // System.out.println(factx);
+
+        //now using the recursion 
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the num : ");
-        char[] x=sc.next().toCharArray();
-        for(int i=x.length-1;i>=0;i--){
-            System.out.print(x[i]);
-        }
-        System.out.print("\nEnter the num : ");
-        String sx=sc.next();
-        StringBuilder sb=new StringBuilder(sx);
-        System.out.println(sb.reverse());
-        System.out.print("\nEnter the num : ");
-        int ab=sc.nextInt();
-        int rev=0;
-        while(ab>0){
-            rev=(rev*10)+(ab%10);
-            ab=ab/10;
-            if(ab<=0){
-                ab=rev;
-                break;
-            }
-            // //or there is an another way 
-            // System.out.print(ab%10);
-            // ab=ab/10;
-        }
-        System.out.println(ab);
+        System.out.print("Enter the n : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the r : ");
+        int r = sc.nextInt();
+        double x=fact(n)/(fact(r)*fact(n-r));
+        System.out.println(x);
         sc.close();
     }
 }
