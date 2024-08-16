393abd68ead269c9b5e7c44acdadadd83753c304
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 17:17:01 +0530

    Pattern Floyd triangle

--- StringChapter.java
+++ StringChapter.java
@@ -3,33 +3,10 @@ import java.util.Scanner;
 
 public class StringChapter {
     public static void pattern(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");           
-            }  
-            for(int k=n-i;k>0;k--){
-                System.out.print("  ");
-            }
-            for(int k=n-i;k>0;k--){
-                System.out.print("  ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        for(int i=n-1;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");           
-            }  
-            for(int k=n-i;k>0;k--){
-                System.out.print("  ");
-            }
-            for(int k=n-i;k>0;k--){
-                System.out.print("  ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
+        int chars=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++,chars++){
+                System.out.print(chars+" ");
             }
             System.out.println();
         }
