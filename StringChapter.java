50fcd40e0dcb2a52769fd270688c3ce096a08e00
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 16:18:45 +0530

    Pattern Hallow Rectangle

--- StringChapter.java
+++ StringChapter.java
@@ -2,20 +2,33 @@
 import java.util.Scanner;
 
 public class StringChapter {
-    public static void pattern(int n){
-        int sym=1;
+    public static void pattern(int n,int m){
         for(int i=0;i<n;i++){
-           for(int j=0;j<=i;j++){
-            System.out.print((sym==0?sym++:sym--)+" ");
-           }
-           System.out.println();
+            if(i==0 || i==n-1){
+                for(int j=0;j<m;j++){
+                    System.out.print("* ");
+                }
+               }
+               else{
+                for(int j=0;j<m;j++){
+                    if(j==0 || j==m-1){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
+                }
+               }
+               System.out.print("\n");
+            }
         }
-    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the n : ");
+        System.out.print("Enter the lenght : ");
         int n = sc.nextInt();
-        pattern(n);
+        System.out.print("Enter the breath : ");
+        int m = sc.nextInt();
+        pattern(n,m);
         sc.close();
     }
 }
