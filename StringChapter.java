4aa2713a6aebf48e50ecb6bc4cc1ed71d67973a1
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 11:31:50 +0530

    Pattern Full pyramid

--- StringChapter.java
+++ StringChapter.java
@@ -2,30 +2,22 @@
 import java.util.Scanner;
 
 public class StringChapter {
-    public static void BinToDec(int n){
-        int sum=0;
-        int i=0;
-        while(n>0){
-            sum=(int) (sum+(n%10*Math.pow(2, i)));
-            n=n/10;
-            i++;
+    public static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int k=n-1;k>i;k--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-        System.out.println(sum);
-    }
-    public static void DecToBin(int n){
-        int Bin=0;
-        while(n>0){
-            Bin = Bin*10 + n%2;
-            n=n/2;
-        }
-        System.out.println(Bin);
     }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the n : ");
         int n = sc.nextInt();
-        // BinToDec(n);
-        DecToBin(n);
+        pattern(n);
         sc.close();
     }
 }
