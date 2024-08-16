287ac9b8930ff7c581279527824ddce58f3ac75e
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 17:02:28 +0530

    Pattern Butterfly shanks

--- StringChapter.java
+++ StringChapter.java
@@ -5,11 +5,29 @@ public class StringChapter {
     public static void pattern(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
+                System.out.print("* ");           
+            }  
+            for(int k=n-i;k>0;k--){
+                System.out.print("  ");
+            }
+            for(int k=n-i;k>0;k--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
         for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");           
+            }  
+            for(int k=n-i;k>0;k--){
+                System.out.print("  ");
+            }
+            for(int k=n-i;k>0;k--){
+                System.out.print("  ");
+            }
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
