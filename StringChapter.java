44cf10d1468af146057d04cab72d87acfb18b99e
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 20:32:42 +0530

    Pattern Hallow Rhombus

--- StringChapter.java
+++ StringChapter.java
@@ -7,9 +7,21 @@ public class StringChapter {
             for(int k=n-i;k>0;k--){
                 System.out.print("  ");
             }
-            for(int j=0;j<n;j++){
-                System.out.print("* ");
+            if(i==0 || i==n-1){
+                for(int j=0;j<n;j++){
+                    System.out.print("* ");
+                }
             }
+            else{
+                for(int j=0;j<n;j++){
+                    if(j==0 || j==n-1){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
+                }
+            } 
             System.out.println();
         }
         
