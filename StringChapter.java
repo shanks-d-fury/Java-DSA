a67b7bf89238410f1b0e23668e4ac26ec39364ee
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 11:09:43 +0530

    Decimal to Binary

--- StringChapter.java
+++ StringChapter.java
@@ -12,11 +12,20 @@ public class StringChapter {
         }
         System.out.println(sum);
     }
+    public static void DecToBin(int n){
+        int Bin=0;
+        while(n>0){
+            Bin = Bin*10 + n%2;
+            n=n/2;
+        }
+        System.out.println(Bin);
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the n : ");
         int n = sc.nextInt();
-        BinToDec(n);
+        // BinToDec(n);
+        DecToBin(n);
         sc.close();
     }
 }
