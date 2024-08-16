fb4f4e9513a073814444f39ef0f18b01aecdba5c
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 20:22:15 +0530

    Pattern solid rhombus

--- StringChapter.java
+++ StringChapter.java
@@ -3,34 +3,16 @@ import java.util.Scanner;
 
 public class StringChapter {
     public static void pattern(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n*2;j++){
-                if(j<=i){
-                    System.out.print("* ");
-                }
-                else  if (j>n*2-i) {
-                    System.out.print("* ");
-                }
-                else{
-                    System.out.print("  ");
-                }
+        for(int i=0;i<n;i++){
+            for(int k=n-i;k>0;k--){
+                System.out.print("  ");
+            }
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
-        for(int i=n-1;i>=0;i--){
-            for(int j=1;j<=n*2;j++){
-                if(j<=i){
-                    System.out.print("* ");
-                }
-                else  if (j>n*2-i) {
-                    System.out.print("* ");
-                }
-                else{
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
+        
     }
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
