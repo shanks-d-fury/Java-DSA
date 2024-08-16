876c355e3f5aea7e79e4c313da50b9aaec4ae0e0
shanks-D-fury <shashanks9190@gmail.com>
2024-08-16 16:27:14 +0530

    Pattern inverted Triangle with Numbers

--- StringChapter.java
+++ StringChapter.java
@@ -2,33 +2,19 @@
 import java.util.Scanner;
 
 public class StringChapter {
-    public static void pattern(int n,int m){
-        for(int i=0;i<n;i++){
-            if(i==0 || i==n-1){
-                for(int j=0;j<m;j++){
-                    System.out.print("* ");
-                }
-               }
-               else{
-                for(int j=0;j<m;j++){
-                    if(j==0 || j==m-1){
-                        System.out.print("* ");
-                    }
-                    else{
-                        System.out.print("  ");
-                    }
-                }
-               }
-               System.out.print("\n");
+    public static void pattern(int n){
+        for(int i=n;i>=0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
+            System.out.println();
         }
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the lenght : ");
+        System.out.print("Enter the n : ");
         int n = sc.nextInt();
-        System.out.print("Enter the breath : ");
-        int m = sc.nextInt();
-        pattern(n,m);
+        pattern(n);
         sc.close();
     }
 }
