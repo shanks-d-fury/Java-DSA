0c4a7a910cf248e911aa930ba435f453418f5f48
shanks-D-fury <shashanks9190@gmail.com>
2024-08-17 22:37:52 +0530

    Array : MaxSubarray using {Kadans algo}

--- Array.java
+++ Array.java
@@ -4,25 +4,27 @@ import java.util.Random;
 public class Array {
 
     public static void subArray(int arr[]) {
-        int prefixArr[] = new int[arr.length];
         int max = Integer.MIN_VALUE;
         int sum = 0;
         int iIndex = 0;
         int jIndex = 0;
-        prefixArr[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            prefixArr[i] = prefixArr[i - 1] + arr[i];
-        }
-        for (int i = 0; i < prefixArr.length; i++) {
-            for (int j = i; j < prefixArr.length; j++) {
-                sum = i == 0 ? prefixArr[j] : prefixArr[j] - prefixArr[i - 1];
-                if (max < sum) {
-                    max = sum;
+        boolean count = false;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum < 0) {
+                sum = 0;
+                count = false;
+            }
+            if (sum > max) {
+                max = sum;
+                jIndex = i;
+                if (max > 0 && count == false) {
                     iIndex = i;
-                    jIndex = j;
                 }
+                count = true;
             }
         }
+        System.out.println("start index : " + iIndex + " " + "End index : " + jIndex);
         System.out.println("The max sub array is :-");
         for (int i = iIndex; i <= jIndex; i++) {
             System.out.print(arr[i] + " ");
@@ -43,8 +45,9 @@ public class Array {
 
     public static void main(String[] args) {
         int num[] = {1, -2, 6, -1, 3};
-        // int[] randomArray = generateRandomArray(5, -100, 100); // Generate an array of 10 integers between 0 and 100
+        int num2[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        int[] randomArray = generateRandomArray(5, -10, 10); // Generate an array of 10 integers between 0 and 100
         // System.out.println(num[4]);
-        subArray(num);
+        subArray(randomArray);
     }
 }
