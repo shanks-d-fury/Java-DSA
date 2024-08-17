004ff7b22eea79d92c844e4ea5dd8b1b5cc64d99
shanks-D-fury <shashanks9190@gmail.com>
2024-08-17 22:52:41 +0530

    Array : Rainwater Trapping using the subArray method ...shanks

--- Array.java
+++ Array.java
@@ -4,32 +4,27 @@ import java.util.Random;
 public class Array {
 
     public static void subArray(int arr[]) {
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
+        int maxArea = Integer.MIN_VALUE;
+        int sumArea = 0;
         int iIndex = 0;
         int jIndex = 0;
-        boolean count = false;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-            if (sum < 0) {
-                sum = 0;
-                count = false;
-            }
-            if (sum > max) {
-                max = sum;
-                jIndex = i;
-                if (max > 0 && count == false) {
+        // boolean count = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                sumArea = j - i * Math.min(arr[i], arr[j]);
+                if (sumArea > maxArea) {
+                    maxArea = sumArea;
                     iIndex = i;
+                    jIndex = j;
                 }
-                count = true;
             }
         }
         System.out.println("start index : " + iIndex + " " + "End index : " + jIndex);
-        System.out.println("The max sub array is :-");
-        for (int i = iIndex; i <= jIndex; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println(" => " + max);
+        System.out.println("The maxArea sub array is :-");
+        // for (int i = iIndex; i <= jIndex; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+        System.out.println(" => " + maxArea);
     }
 
     public static int[] generateRandomArray(int size, int min, int max) {
@@ -46,8 +41,9 @@ public class Array {
     public static void main(String[] args) {
         int num[] = {1, -2, 6, -1, 3};
         int num2[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-        int[] randomArray = generateRandomArray(5, -10, 10); // Generate an array of 10 integers between 0 and 100
+        int num3[] = {4, 2, 0, 6, 3, 2, 5};
+        int randomArray[] = generateRandomArray(5, -10, 10); // Generate an array of 10 integers between 0 and 100
         // System.out.println(num[4]);
-        subArray(randomArray);
+        subArray(num3);
     }
 }
