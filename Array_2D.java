a5ce5e2d1a69f67a94a97e93938f47a76c2a7bf5
shanks-D-fury <shashanks9190@gmail.com>
2024-08-27 20:47:30 +0530

    2D-Array : 1-0-1 Matrix {with some extra changes} ...shanks

--- Array_2D.java
+++ Array_2D.java
@@ -3,19 +3,8 @@ import java.util.Scanner;
 
 public class Array_2D {
 
-    public static int[][] randomMatrix(int n, int m) {
-        int randomMatrix[][] = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                //This is common way to write the {Math.random*(max-min+1)+ min} ;
-                randomMatrix[i][j] = (int) (Math.random() * (9 - 1 + 1)) + 1;
-            }
-        }
-        return randomMatrix;
-    }
-
     public static void printMatrix(int matrix[][]) {
-        System.out.println("The 0-1-0 matrix is : ");
+        System.out.println("The 1-0-1 matrix is : ");
         for (int[] matrix1 : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix1[j] + " ");
@@ -30,39 +19,33 @@ public class Array_2D {
         int endRow = matrix.length - 1;
         int startCol = 0;
         int endCol = matrix[0].length - 1;
-        int k = 1;
+        // int k = matrix.length - 1; // For decreasing Matrix 
+        int k = 1; // for the 0-1-0 Matrix for 1-0-1 Matrix give k=1;
         while (startRow <= endRow && startCol <= endCol) {
             //Top
             for (int j = startCol; j <= endCol; j++) {
-                // System.out.print(matrix[startRow][j] + " ");
                 matrix[startRow][j] = k;
             }
             //Right
             for (int i = startRow + 1; i <= endRow; i++) {
-                // System.out.print(matrix[i][endCol] + " ");
                 matrix[i][endCol] = k;
             }
             //Bottom
             for (int j = endCol - 1; j >= startCol; j--) {
-                // if (startRow == endRow) {
-                //     break;
-                // }
-                // System.out.print(matrix[endRow][j] + " ");
                 matrix[endRow][j] = k;
             }
             //Left
             for (int i = endRow - 1; i >= startRow + 1; i--) {
-                // if (startCol == endCol) {
-                //     break;
-                // }
-                // System.out.print(matrix[i][startCol] + " ");
                 matrix[i][startCol] = k;
             }
             startCol++;
             startRow++;
             endCol--;
             endRow--;
-            k = changeNum(k);
+            k = changeNum(k); // 0-1-0 Matrix
+            // k -= 2; //odd changing Matrix
+            // k--; // Simple dicreasing matrix
+
         }
         System.out.println();
     }
@@ -79,8 +62,6 @@ public class Array_2D {
             int n = sc.nextInt();
             System.out.print("Enter m (Columns): ");
             int m = sc.nextInt();
-            // int matrix[][] = randomMatrix(n, m);
-            // printMatrix(matrix);
             int matrix[][] = new int[n][m];
             SprialMatrix_0_1_0(matrix);
             printMatrix(matrix);
