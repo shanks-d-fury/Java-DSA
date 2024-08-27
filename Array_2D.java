b41eeb109be0ebc913052901e41a79c9a9b5885a
shanks-D-fury <shashanks9190@gmail.com>
2024-08-27 22:42:52 +0530

    2D-Array : Diagonal sum optimized ...shanks

--- Array_2D.java
+++ Array_2D.java
@@ -20,31 +20,39 @@ public class Array_2D {
             for (int j = 0; j < m; j++) {
                 //This is common way to write the {Math.random*(max-min+1)+ min} ;
                 randomMatrix[i][j] = (int) (Math.random() * (9 - 1 + 1)) + 1;
+                // randomMatrix[i][j] = 1;
             }
         }
         return randomMatrix;
     }
 
     public static void DiagonalSum(int matrix[][]) {
-        int sum = 0;
-        int i, j;
-        int n = matrix.length - 1;
-        int m = matrix[0].length - 1;
-        for (i = 0, j = 0; i <= n && j <= m; j++, i++) {
-            sum += matrix[i][j];
+        try {
+            int sum = 0;
+            int i, j;
+            int n = matrix.length;
+            int m = matrix[0].length;
+            for (i = 0, j = 0; i < n && j < m; j++, i++) {
+                sum += matrix[i][j];
+            }
+
+            for (i = 0, j = m - 1; i < n && j >= 0; j--, i++) {
+                if (i == j) {
+                    continue;
+                }
+                sum += matrix[i][j];
+            }
+            System.out.println("The sum is : " + sum + "\n");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("The sum is : -1");
         }
 
-        for (i = 0, j = m; i <= n && j >= 0; j--, i++) {
-            if (i == j) {
-                continue;
-            }
-            sum += matrix[i][j];
-        }
-        System.out.println("The sum is : " + sum + "\n");
     }
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+            int mat[] = {0};
+            System.out.println(mat.length);
             System.out.print("Enter n: ");
             int n = sc.nextInt();
             int matrix[][] = randomMatrix(n, n);
