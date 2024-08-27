c188e88f67ddda8f66313a0c541ecad4a2e03de1
shanks-D-fury <shashanks9190@gmail.com>
2024-08-28 00:42:30 +0530

    2D-Array : Transpose Matrix {O(n^2){ ...shanks

--- Array_2D.java
+++ Array_2D.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class Array_2D {
 
-    public static void printMatrix(int matrix[][]) {
-        System.out.println("Matrix : ");
+    public static void printMatrix(int matrix[][], String s) {
+        System.out.println(s + "Matrix : ");
         for (int[] matrix1 : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix1[j] + " ");
@@ -14,55 +14,38 @@ public class Array_2D {
         System.out.println();
     }
 
-    public static void printTestMatrix(int matrix[][]) {
-        System.out.println("Matrix : ");
-        for (int[] matrix1 : matrix) {
-            System.out.print("{");
-            for (int j = 0; j < matrix[0].length; j++) {
-                String S = j != matrix[0].length - 1 ? "," : "";
-                System.out.print(matrix1[j] + S);
-            }
-            System.out.println("},");
-        }
-        System.out.println();
-    }
-
-    public static int[][] randomSortedMatrix(int n, int m) {
+    public static int[][] randomMatrix(int n, int m) {
         int randomMatrix[][] = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) {
-                    randomMatrix[i][j] = (int) (Math.random() * (10 - 10 + 1)) + 10;
-                    continue;
-                }
-                if (i != 0 && j == 0) {
-                    randomMatrix[i][j] = randomMatrix[i - 1][j] + (int) (Math.random() * (10 - randomMatrix[i - 1][j] + 1)) + randomMatrix[i - 1][j];
-                    continue;
-                }
-                randomMatrix[i][j] = randomMatrix[i][j - 1] + (int) (Math.random() * (10 - randomMatrix[i][j - 1] + 1)) + randomMatrix[i][j - 1];
+                //This is common way to write the {Math.random*(max-min+1)+ min} ;
+                randomMatrix[i][j] = (int) (Math.random() * (9 - 1 + 1)) + 1;
+                // randomMatrix[i][j] = 1;
             }
         }
         return randomMatrix;
     }
 
-    public static int[] SortedMatrixScreach(int matrix[][], int key) {
+    public static int[][] TransposeMatrix(int matrix[][]) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int i = 0, j = m - 1;
-        int retMat[] = new int[3];
-        while (i < n && j >= 0) {
-            if (matrix[i][j] == key) {
-                retMat[0] = 1;
-                retMat[1] = i;
-                retMat[2] = j;
-                return retMat;
-            } else if (key < matrix[i][j]) {
-                j--;
-            } else if (key > matrix[i][j]) {
-                i++;
+        if (n == m) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < m; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+            return matrix;
+        }
+        int TransposeMatrix[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                TransposeMatrix[i][j] = matrix[j][i];
             }
         }
-        return retMat;
+        return TransposeMatrix;
     }
 
     public static void main(String[] args) {
@@ -71,21 +54,10 @@ public class Array_2D {
             int n = sc.nextInt();
             System.out.print("Enter m (Columns): ");
             int m = sc.nextInt();
-            int matrix[][] = randomSortedMatrix(n, m);
-            printMatrix(matrix);
-
-            // //For Testing a matrix use this 
-            // int matrix[][] = {{10, 20, 33, 48},
-            // {20, 34, 55, 93},
-            // {34, 56, 70, 122},
-            // {48, 72, 122, 164},};
-            // //comment out this below line to get a test matrix in printed form
-            // printTestMatrix(matrix);
-            //Enter the key for both the random and non-random matrix
-            System.out.print("Enter key : ");
-            int key = sc.nextInt();
-            int AnsMat[] = SortedMatrixScreach(matrix, key);
-            System.out.println(AnsMat[0] == 1 ? "Keyfound At (" + (AnsMat[1] + 1) + "," + (AnsMat[2] + 1) + ")" : "Notfound");
+            int matrix[][] = randomMatrix(n, m);
+            printMatrix(matrix, "Random ");
+            int TarnsposeMatrix[][] = TransposeMatrix(matrix);
+            printMatrix(TarnsposeMatrix, "Transpose ");
         }
     }
 }
