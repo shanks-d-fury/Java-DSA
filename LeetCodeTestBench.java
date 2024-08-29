27a1a83a1b0b10987788e24a74402b11f2dfd52c
shanks-D-fury <shashanks9190@gmail.com>
2024-08-29 23:52:51 +0530

    LeetCode : Merge Alternately (String) {O(n)} ...shanks

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,32 +1,27 @@
 
 public class LeetCodeTestBench {
 
-    public static int findClosestNumber(int[] nums) {
-        // This is my original code but some partial answer
-        int min = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            int x = nums[i];
-            if (x < 0) {
-                min = min < 0 ? Math.max(min, x) : (min + x == 0 ? min : Math.min(min, x));
-            } else {
-                min = min < 0 ? Math.max(min, x) : Math.min(min, x);
-            }
+    public static String mergeAlternately(String word1, String word2) {
+        int i;
+        String s = new String();
+        char[] word1x = word1.toCharArray();
+        char[] word2x = word2.toCharArray();
+        int min = Math.min(word1x.length, word2x.length);
+        for (i = 0; i < min; i++) {
+            s += word1x[i];
+            s += word2x[i];
         }
-        return min;
-
-        // // The solution code is 
-        // int min = Integer.MAX_VALUE;
-        // for (int x : nums) {
-        //     if (x + min == 0) {
-        //         min = min > 0 ? min : x;
-        //     } else if (Math.abs(min) > Math.abs(x)) {
-        //         min = x;
-        //     }
-        // }
+        if (word1x.length > word2x.length) {
+            s += word1.substring(i, word1.length());
+        } else {
+            s += word2.substring(i, word2.length());
+        }
+        // s+=word1.substring(i,word1.length());
+        return s;
     }
 
     public static void main(String[] args) {
-        int testAry[] = {2, 1, 1, -1, 100000};
-        System.out.println(findClosestNumber(testAry));
+        // int testAry[] = {2, 1, 1, -1, 100000};
+        System.out.println(mergeAlternately("ab", "pqrsss"));
     }
 }
