2649326b93a987fe9956082d983d0b11e19519a8
shanks-D-fury <shashanks9190@gmail.com>
2024-08-30 21:51:31 +0530

    Strings : String Palindrome using StringBuilder ...shanks

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,42 +1,12 @@
 
-import java.util.Arrays;
-
 public class LeetCodeTestBench {
 
-    public static String findLargestString(String[] s) {
-        String largestString = "";
-        for (String sx : s) {
-            largestString = largestString.compareToIgnoreCase(sx) >= 0 ? largestString : sx;
-        }
-        return largestString;
+    public static boolean stringPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return s.equals(sb.reverse().toString());
     }
 
     public static void main(String[] args) {
-        String[] s = {"zebra",
-            "zinc",
-            "zone",
-            "zero",
-            "zombie",
-            "zipper",
-            "zephyr",
-            "zigzag",
-            "zucchini",
-            "zeppelin",
-            "zenith",
-            "zest",
-            "zodiac",
-            "zombie",
-            "zoology",
-            "zoom",
-            "zesty",
-            "zen",
-            "zookeeper",
-            "ziggurat"};
-        System.out.println(findLargestString(s));
-
-        //Alternative for the above function is 
-        Arrays.sort(s);
-        System.out.println(s[s.length - 1]);
-
+        System.out.println(stringPalindrome("121"));
     }
 }
