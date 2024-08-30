c91fb845ae056274ff147ce9b9b6d5a0384e52e1
shanks-D-fury <shashanks9190@gmail.com>
2024-08-30 23:55:02 +0530

    String : Decimal to Binery { Integer.toBineryString(dec)} ...shanks

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,19 +1,12 @@
 
-import java.util.Arrays;
-
 public class LeetCodeTestBench {
 
-    public static boolean checkAnagram(String s1, String s2) {
-        char[] c1 = s1.toCharArray();
-        char[] c2 = s2.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        return Arrays.equals(c1, c2);
+    public static String DecimalToBinary(int decimal) {
+        return Integer.toBinaryString(decimal);
     }
 
     public static void main(String[] args) {
-        String s1 = "mama";
-        String s2 = "amma";
-        System.out.println(checkAnagram(s1, s2));
+        int decimal = 2024;
+        System.out.println("Decimal : " + decimal + "\nBinery : " + DecimalToBinary(2024));
     }
 }
