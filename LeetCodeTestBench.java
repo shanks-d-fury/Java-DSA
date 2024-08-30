ebcc42efca39ac5657418c2a650e3112a2fb9593
shanks-D-fury <shashanks9190@gmail.com>
2024-08-30 23:08:02 +0530

    Strings : Strin g compresion {Edge case and simple code AP}

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -2,32 +2,21 @@
 public class LeetCodeTestBench {
 
     public static String stringComprestion(String s) {
-        int count = 1;
         StringBuilder sb = new StringBuilder();
-        //Edge case of single letter
-        if (s.length() == 1) {
-            sb.append(s.charAt(0));
-            sb.append(count);
-            return sb.toString();
-        }
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) != s.charAt(i - 1)) {
-                sb.append(s.charAt(i - 1));
-                sb.append(count);
-                count = 1;
-            } else {
+        for (int i = 0; i < s.length(); i++) {
+            int count = 1;
+            while (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
                 count++;
+                i++;
             }
-            if (i == s.length() - 1) {
-                sb.append(s.charAt(i));
-                sb.append(count);
-            }
+            sb.append(s.charAt(i));
+            sb.append(count);
         }
         return sb.toString();
     }
 
     public static void main(String[] args) {
-        String s = "jjasooool;sjsssss";
+        String s = "";
         System.out.println(stringComprestion(s));
     }
 }
