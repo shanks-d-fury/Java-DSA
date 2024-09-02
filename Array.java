eb1af21cd46190a62d6d61f90e877e06e860afd6
shanks-D-fury <shashanks9190@gmail.com>
2024-09-03 02:16:22 +0530

    Array : Buy & Sell Stocks {best solution O(n) beats 99.7%} ...shanks
    found the BuyDay using the formula {BuyDay=MaxProfit-StockArray[sellDay-1]}

--- Array.java
+++ Array.java
@@ -8,27 +8,36 @@ public class Array {
             System.out.print(x + ",");
         }
         System.out.println();
-        int maxprofit = 0;
-        int buyDay = 0, sellDay = 0;
-        for (int i = 0; i < StockArray.length - 1; i++) {
-            for (int j = i + 1; j < StockArray.length; j++) {
-                if (StockArray[j] - StockArray[i] > maxprofit) {
-                    maxprofit = StockArray[j] - StockArray[i];
-                    buyDay = i;
-                    sellDay = j;
+        int maxProfit = 0, Profit;
+        int buyPrice = StockArray[0], sellDay = 1, buyDay;
+        for (int i = 1; i < StockArray.length; i++) {
+            Profit = StockArray[i] - buyPrice;
+            if (StockArray[i] > buyPrice) {
+                if (Profit > maxProfit) {
+                    maxProfit = Profit;
+                    sellDay = i + 1;
                 }
+            } else {
+                buyPrice = StockArray[i];
             }
         }
-        buyDay++;
-        sellDay++;
-        System.out.println("Buy & Sell ==> " + buyDay + "," + sellDay + " Profit: " + maxprofit);
-        return maxprofit;
+        buyDay = findDayByValue(StockArray, StockArray[sellDay - 1] - maxProfit);
+        System.out.println("Buy & Sell ==> " + buyDay + "," + sellDay + " Profit: " + maxProfit);
+        return maxProfit;
+    }
+
+    public static int findDayByValue(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i + 1;
+            }
+        }
+        return 0;
     }
 
     public static int[] generateRandomArray(int size, int min, int max) {
         Random rand = new Random();
         int[] array = new int[size];
-
         for (int i = 0; i < size; i++) {
             array[i] = rand.nextInt((max - min) + 1) + min;
         }
@@ -37,12 +46,12 @@ public class Array {
     }
 
     public static void main(String[] args) {
-        // int num[] = {1, -2, 6, -1, 3};
-        // int num2[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        // int num[] = {7, 6, 4, 3, 1};
+        // int num2[] = {9, 2, 1, 8, 10, 4, 9, 8, 0, 3};
         // int num3[] = {4, 2, 0, 6, 3, 2, 5}; //Apna college questions example
         // int num4[] = {7, 1, 5, 3, 6, 4};
-        int num5[] = {1, 5, 3, 5, 9, 2, 0, 5, 1, 7};
-        // int randomArray[] = generateRandomArray(10, 0, 10); // Generate an array of 10 integers between 0 and 100
-        Buy_Sell_Stock(num5);
+        // int num5[] = {1, 5, 3, 5, 9, 2, 0, 5, 1, 7};
+        int randomArray[] = generateRandomArray(100, 0, 100); // Generate an array of 10 integers between 0 and 100
+        System.out.println(Buy_Sell_Stock(randomArray));
     }
 }
