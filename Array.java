b99808dc3a0356805a93978fa32c1f237d7f43cf
shanks-D-fury <shashanks9190@gmail.com>
2024-09-03 00:41:19 +0530

    Array : Buy and Sell Stock {solved : O{n^2) and found the Days} ...shanks

--- Array.java
+++ Array.java
@@ -3,41 +3,26 @@ import java.util.Random;
 
 public class Array {
 
-    public static void Buy_Sell_Stock(int StockArray[]) {
+    public static int Buy_Sell_Stock(int StockArray[]) {
         for (int x : StockArray) {
             System.out.print(x + ",");
         }
         System.out.println();
-        int n = StockArray.length;
-        int curBuyDay = 0;
-        int BuyDay = 1;
-        int SellDay = 0;
-
-        int SellPrice;
-        int BuyPrice = Integer.MAX_VALUE;
-        int Profit = 0;
-        int MaxProfit = 0;
-        int MaxProfitArray[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            SellPrice = StockArray[i];
-            Profit = SellPrice - BuyPrice;
-            if (BuyPrice < SellPrice) {
-                if (Profit > MaxProfit) {
-                    MaxProfit = Profit;
-                    SellDay = i + 1;
-                    MaxProfitArray[i] = MaxProfit;
-                }
-            } else {
-                BuyPrice = SellPrice;
-                BuyDay = i + 1;
-                if (Profit > 0) {
-                    BuyDay = i + 1;
+        int maxprofit = 0;
+        int buyDay = 0, sellDay = 0;
+        for (int i = 0; i < StockArray.length - 1; i++) {
+            for (int j = i + 1; j < StockArray.length; j++) {
+                if (StockArray[j] - StockArray[i] > maxprofit) {
+                    maxprofit = StockArray[j] - StockArray[i];
+                    buyDay = i;
+                    sellDay = j;
                 }
             }
-            System.out.println("B: " + BuyDay + " S: " + SellDay + " MP: " + MaxProfit + " Pro: " + Profit + " SP: " + SellPrice + " BP: " + BuyPrice);
         }
-        // System.out.println("B: " + BuyDay + " S: " + SellDay + " MP: " + MaxProfit);
+        buyDay++;
+        sellDay++;
+        System.out.println("Buy & Sell ==> " + buyDay + "," + sellDay + " Profit: " + maxprofit);
+        return maxprofit;
     }
 
     public static int[] generateRandomArray(int size, int min, int max) {
@@ -52,17 +37,12 @@ public class Array {
     }
 
     public static void main(String[] args) {
-        int num[] = {1, -2, 6, -1, 3};
-        int num2[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-        int num3[] = {4, 2, 0, 6, 3, 2, 5};//Apna college questions example
-        int num4[] = {7, 1, 5, 3, 6, 4};
+        // int num[] = {1, -2, 6, -1, 3};
+        // int num2[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        // int num3[] = {4, 2, 0, 6, 3, 2, 5}; //Apna college questions example
+        // int num4[] = {7, 1, 5, 3, 6, 4};
         int num5[] = {1, 5, 3, 5, 9, 2, 0, 5, 1, 7};
-        int randomArray[] = generateRandomArray(10, 0, 10); // Generate an array of 10 integers between 0 and 100
-        Buy_Sell_Stock(randomArray);
-    }
-
-    public static void sort(String[] s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+        // int randomArray[] = generateRandomArray(10, 0, 10); // Generate an array of 10 integers between 0 and 100
+        Buy_Sell_Stock(num5);
     }
 }
