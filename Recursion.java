60abece3f87bbe8b00202a819279a5c4dafa79f3
shanks-D-fury <shashanks9190@gmail.com>
2024-09-10 15:34:08 +0530

    Recursion : fabanoci of nth term & fabanoci series

--- Recursion.java
+++ Recursion.java
@@ -3,7 +3,8 @@ public class Recursion {
 
     public static void main(String[] args) {
         // System.out.println(recursionfact(4));
-        IncOrd(20);
+        // fabanoci(3);
+        fabanociSeries(10);
     }
 
     public static void IncOrd(int n) {
@@ -20,5 +21,23 @@ public class Recursion {
             return 1;
         }
         return n * recursionfact(n - 1);
+    }
+
+    public static int fabanoci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int n1 = fabanoci(n - 1);
+        int n2 = fabanoci(n - 2);
+        return n1 + n2;
+    }
+
+    public static void fabanociSeries(int n) {
+        if (n == 0) {
+            System.out.println("fabnoci(" + n + ") => " + fabanoci(n));
+            return;
+        }
+        fabanociSeries(n - 1);
+        System.out.println("fabnoci(" + n + ") => " + fabanoci(n));
     }
 }
