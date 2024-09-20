a67f6f85ba15c57c9f66cb79921044517ad1eac5
shanks-D-fury <shashanks9190@gmail.com>
2024-09-20 15:06:38 +0530

    Recursion : Tiling problem {2x1}

--- Recursion.java
+++ Recursion.java
@@ -2,77 +2,17 @@
 public class Recursion {
 
     public static void main(String[] args) {
-        // System.out.println(recursionfact(4));
-        // fabanoci(3);
-        // System.out.println(binetFabanoci(10));
         long startTime = System.currentTimeMillis();
-        // fabanociSeries(45);
-        // int arr[] = {15, 2, 10, 14, 15, 20, 16, 201, 15, 15};
-        System.out.println(xPowerN(2, 20));
-        System.out.println(Math.pow(2, 20));
+        System.out.println(tails2_1(4));
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
         System.out.println("Time taken : " + timeTaken + " ms");
     }
 
-    public static void IncOrd(int n) {
-        if (n == 1) {
-            System.out.println("fact(" + n + ") => " + recursionfact(n));
-            return;
-        }
-        IncOrd(n - 1);
-        System.out.println("fact(" + n + ") => " + recursionfact(n));
-    }
-
-    public static long recursionfact(int n) {
+    public static int tails2_1(int n) {
         if (n == 0 || n == 1) {
             return 1;
         }
-        return n * recursionfact(n - 1);
-    }
-
-    public static int fabanoci(int n) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-        int n1 = fabanoci(n - 1);
-        int n2 = fabanoci(n - 2);
-        return n1 + n2;
-    }
-
-    public static void fabanociSeries(int n) {
-        if (n == 0) {
-            System.out.println("fabnoci(" + n + ") => " + binetFabanoci(n));
-            return;
-        }
-        fabanociSeries(n - 1);
-        System.out.println("fabnoci(" + n + ") => " + binetFabanoci(n));
-    }
-
-    public static int binetFabanoci(int n) {
-        int ans;
-        float theta = (float) ((1 + Math.sqrt(5)) / 2);
-        float py = (float) ((1 - Math.sqrt(5)) / 2);
-        ans = (int) ((int) (Math.pow(theta, n) + Math.pow(py, n)) / Math.sqrt(5));
-        return ans;
-    }
-
-    public static int lastOccurance(int arr[], int i, int key) {
-        if (arr[i] == key) {
-            return i;
-        }
-        if (i < 0) {
-            return -1;
-        }
-        return lastOccurance(arr, i - 1, key);
-
-    }
-
-    public static int xPowerN(int x, int n) {
-        if (n == 0) {
-            return 1;
-        }
-        int power = xPowerN(x, n / 2);
-        return n % 2 == 0 ? power * power : x * power * power;
+        return tails2_1(n - 1) + tails2_1(n - 2);
     }
 }
