1d7682e265ea41587947b5c6aa4fba04af4f471a
shanks-D-fury <shashanks9190@gmail.com>
2024-09-23 12:32:13 +0530

    LeetCode : Longest common prefix {beats 11.5% }
    still the solution can be improved by using the substring & index

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -3,16 +3,53 @@ import java.util.Arrays;
 public class LeetCodeTestBench {
 
     public static void main(String[] args) {
-        String testArray[] = { "slow", "slower", "slowest" };
+        String testArray[] = { "introspection", // because of this the longest common prefix is "int";
+                "intermediate",
+                "internet",
+                "interval",
+                "internal",
+                "interlock",
+                "interaction",
+                "intercept",
+                "interesting",
+                "international",
+                "interpret",
+                "interstate",
+                "intervene",
+                "intermittent",
+                "interceptive",
+                "interchangeable",
+                "interpolate",
+                "intermediate",
+                "interrelation",
+                "interrogation" };
+        // printAry(testArray);
+        //
+        long startTime = System.currentTimeMillis();
+        //
         System.out.println(longestCommonPrefix(testArray));
+        //
+        long endTime = System.currentTimeMillis();
+        long timeTaken = endTime - startTime;
+        System.out.println("Time taken : " + timeTaken + " ms");
+
+    }
+
+    public static void printAry(String ary[]) {
+        for (String x : ary) {
+            System.out.println(x);
+        }
+        System.out.println();
     }
 
     public static String longestCommonPrefix(String[] strs) {
         String lcp = new String();
         Arrays.sort(strs);
-        String mString = Arrays.stream(strs).min((s1, s2) -> Integer.compare(s1.length(), s2.length())).orElse(null);
-        for (int i = 0; i < mString.length(); i++) {
-            if (strs[0].charAt(i) != strs[strs.length - 1].charAt(i)) {
+        // printAry(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
                 return lcp;
             }
             lcp += strs[0].charAt(i);
