ba0bc1e6256973da54ec28f762e6525adab3f4af
shanks-D-fury <shashanks9190@gmail.com>
2024-09-23 12:04:01 +0530

    LeetCode : Longest common prefix { shanks beats 6.91%}

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,12 +1,28 @@
+import java.util.Arrays;
 
 public class LeetCodeTestBench {
 
-    public static String DecimalToBinary(int decimal) {
-        return Integer.toBinaryString(decimal);
+    public static void main(String[] args) {
+        String testArray[] = { "slow", "slower", "slowest" };
+        System.out.println(longestCommonPrefix(testArray));
     }
 
-    public static void main(String[] args) {
-        int decimal = 2024;
-        System.out.println("Decimal : " + decimal + "\nBinery : " + DecimalToBinary(2024));
+    public static String longestCommonPrefix(String[] strs) {
+        String lcp = new String();
+        int i = 0;
+        String minString = Arrays.stream(strs)
+                .min((s1, s2) -> Integer.compare(s1.length(), s2.length()))
+                .orElse(null);
+        while (i < minString.length()) {
+            char x = minString.charAt(i);
+            for (String str : strs) {
+                if (x != str.charAt(i)) {
+                    return lcp;
+                }
+            }
+            lcp += x;
+            i++;
+        }
+        return lcp;
     }
 }
