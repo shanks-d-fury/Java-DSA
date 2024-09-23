9a647b44f5eefe9bd53ab8a5cdd4c7d211b4f7be
shanks-D-fury <shashanks9190@gmail.com>
2024-09-23 12:16:12 +0530

    LeetCode : Longest common prefix { due to the Arrays.stream the Time Complexity is slow }

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -9,19 +9,13 @@ public class LeetCodeTestBench {
 
     public static String longestCommonPrefix(String[] strs) {
         String lcp = new String();
-        int i = 0;
-        String minString = Arrays.stream(strs)
-                .min((s1, s2) -> Integer.compare(s1.length(), s2.length()))
-                .orElse(null);
-        while (i < minString.length()) {
-            char x = minString.charAt(i);
-            for (String str : strs) {
-                if (x != str.charAt(i)) {
-                    return lcp;
-                }
+        Arrays.sort(strs);
+        String mString = Arrays.stream(strs).min((s1, s2) -> Integer.compare(s1.length(), s2.length())).orElse(null);
+        for (int i = 0; i < mString.length(); i++) {
+            if (strs[0].charAt(i) != strs[strs.length - 1].charAt(i)) {
+                return lcp;
             }
-            lcp += x;
-            i++;
+            lcp += strs[0].charAt(i);
         }
         return lcp;
     }
