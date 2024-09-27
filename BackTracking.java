ebcdd6664c6025759c9d2ad71a8a7d112893e96c
shanks-D-fury <shashanks9190@gmail.com>
2024-09-27 11:56:03 +0530

    BackTracking : GridWays { O(2^(n+M)) }

--- BackTracking.java
+++ BackTracking.java
@@ -1,91 +1,21 @@
 public class BackTracking {
-    static int count = 0;
-
-    public static boolean isSafe(char board[][], int row, int col) {
-        // top vertical
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
+    public static int gridWays(int n, int m, int i, int j) {
+        if (i == n - 1 && j == m - 1) {
+            return 1;
         }
-        // top left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
+        if (i > n || j > m) {
+            return 0;
         }
-        // top right diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean nQueens(char board[][], int row) {
-        if (row == board.length) {
-            // printBoard(board);
-            count++;
-            return true;
-        }
-        for (int j = 0; j < board.length; j++) {
-            if (isSafe(board, row, j)) {
-                board[row][j] = 'Q';
-                if (nQueens(board, row + 1)) {
-                    return true;
-                }
-                board[row][j] = 'x';
-            }
-        }
-        return false;
-    }
-
-    public static void printBoard(char board[][]) {
-        System.out.println("\t------ Board ------");
-        System.out.print("\t    ");
-        for (int i = 1; i <= board.length; i++) {
-            System.out.print(i + " | ");
-        }
-        System.out.println();
-        System.out.print("\t  ");
-        for (int i = 1; i <= board.length; i++) {
-            System.out.print(" __ ");
-        }
-        System.out.println();
-        int i = 1;
-        for (char[] board1 : board) {
-            System.out.print("\t" + i++ + " | ");
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board1[j] + " | ");
-            }
-            System.out.println();
-            System.out.print("\t  ");
-            for (int k = 0; k < board.length; k++) {
-                System.out.print(" __ ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void initilzeBoard(char board[][]) {
-        for (char[] board1 : board) {
-            for (int j = 0; j < board.length; j++) {
-                board1[j] = 'x';
-            }
-        }
+        return gridWays(n, m, i + 1, j) + gridWays(n, m, i, j + 1);
     }
 
     public static void main(String args[]) {
-        int n = 9;
-        char board[][] = new char[n][n];
-        initilzeBoard(board);
+
         long startTime = System.currentTimeMillis();
         //
-        if (nQueens(board, 0)) {
-            printBoard(board);
-        }
-        System.out.println("\n" + count);
+        int n = 5, m = 5;
+        System.out.println(gridWays(n, m, 0, 0));
+
         //
         long endTime = System.currentTimeMillis();
         //
