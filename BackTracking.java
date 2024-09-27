e3cd1cf1915ccd950eb7215de1c158322547dcff
shanks-D-fury <shashanks9190@gmail.com>
2024-09-27 12:41:53 +0530

    BackTracking : GridWays using factorial {O(n)}

--- BackTracking.java
+++ BackTracking.java
@@ -9,13 +9,25 @@ public class BackTracking {
         return gridWays(n, m, i + 1, j) + gridWays(n, m, i, j + 1);
     }
 
+    public static long fact(long n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+
     public static void main(String args[]) {
 
         long startTime = System.currentTimeMillis();
         //
-        int n = 5, m = 5;
-        System.out.println(gridWays(n, m, 0, 0));
+        long n = 10, m = 10;
+        // the grid ways method has a huge time complexity O(2^n+m);
+        // System.out.println(gridWays(n, m, 0, 0));
+        // to decrese the time complexity we use a simple permutation formula
 
+        // The fact is a linear time complexity so we use it insted of the gridways
+        // formula
+        System.out.println(fact((n - 1 + m - 1)) / (fact((n - 1)) * fact((m - 1))));
         //
         long endTime = System.currentTimeMillis();
         //
