3d4845c089cffb6029bb875a0aae81b05c857c73
shanks-D-fury <shashanks9190@gmail.com>
2024-10-09 23:04:58 +0530

    Stack : Reverse a Stack

--- StacksChapter.java
+++ StacksChapter.java
@@ -25,9 +25,25 @@ public class StacksChapter {
         return s.toString();
     }
 
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushBottom(s, top);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
-        System.out.println(reverseString(""));
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        reverseStack(s);
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
         // System.out.println(s);
     }
 }
