304d2150063eee326a9b429566b3658826071eb4
shanks-D-fury <shashanks9190@gmail.com>
2024-10-16 11:53:50 +0530

    Queue : InterLeave 2 halves of queue { AP method ... used only queues}

--- QueueChapter.java
+++ QueueChapter.java
@@ -3,28 +3,30 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueChapter {
-    public static String interLeave_2_halves(int[] ary) {
+    public static Queue<Integer> interLeave_2_halves(Queue<Integer> q) {
+        int i = 0;
+        int size = q.size();
         Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        for (int i = 0; i < ary.length; i++) {
-            if (i < ary.length / 2) {
-                q1.add(ary[i]);
-            } else {
-                q2.add(ary[i]);
-            }
+        while (i < size / 2) {
+            q1.add(q.remove());
+            i++;
         }
-        StringBuilder sx = new StringBuilder();
-        while (!q1.isEmpty() && !q2.isEmpty()) {
-            sx.append(q1.remove());
-            sx.append(" ");
-            sx.append(q2.remove());
-            sx.append(" ");
+        while (!q1.isEmpty()) {
+            q.add(q1.remove());
+            q.add(q.remove());
         }
-        return sx.toString();
+        return q;
     }
 
     public static void main(String args[]) {
         int input[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        System.out.println(interLeave_2_halves(input));
+        Queue<Integer> qx = new LinkedList<>();
+        for (int x : input) {
+            qx.add(x);
+        }
+        Queue<Integer> q = interLeave_2_halves(qx);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
     }
 }
