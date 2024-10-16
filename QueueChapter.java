b60fc9d9558cd52591e46d3214e8b58796a5d7d1
shanks-D-fury <shashanks9190@gmail.com>
2024-10-16 11:40:36 +0530

    Queue : InterLeave 2 halves problem

--- QueueChapter.java
+++ QueueChapter.java
@@ -3,25 +3,28 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueChapter {
-    public static void firstNonRepetingChar(String s) {
-        int freq[] = new int[26];
-        Queue<Character> q = new LinkedList<>();
-        for (char c : s.toCharArray()) {
-            int idx = c - ' ' - 65;
-            freq[idx]++;
-            q.add(c);
-            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
-                q.remove();
-            }
-            if (q.isEmpty()) {
-                System.out.print(-1 + " ");
+    public static String interLeave_2_halves(int[] ary) {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        for (int i = 0; i < ary.length; i++) {
+            if (i < ary.length / 2) {
+                q1.add(ary[i]);
             } else {
-                System.out.print(q.peek() + " ");
+                q2.add(ary[i]);
             }
         }
+        StringBuilder sx = new StringBuilder();
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            sx.append(q1.remove());
+            sx.append(" ");
+            sx.append(q2.remove());
+            sx.append(" ");
+        }
+        return sx.toString();
     }
 
     public static void main(String args[]) {
-        firstNonRepetingChar("aabccdb");
+        int input[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println(interLeave_2_halves(input));
     }
 }
