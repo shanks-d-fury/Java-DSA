e0fc0244b0bf83e837c4a7c018dae780482eaf78
shanks-D-fury <shashanks9190@gmail.com>
2024-10-16 11:21:39 +0530

    Queue : First Non repeating Character

--- QueueChapter.java
+++ QueueChapter.java
@@ -3,80 +3,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueChapter {
-    public static class Stacks {
-        static Queue<Integer> q1 = new LinkedList<>();
-        static Queue<Integer> q2 = new LinkedList<>();
-
-        public static boolean isEmpty() {
-            return q1.isEmpty() && q2.isEmpty();
-        }
-
-        public static void push(int data) {
-            if (!q1.isEmpty()) {
-                q1.add(data);
+    public static void firstNonRepetingChar(String s) {
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            int idx = c - ' ' - 65;
+            freq[idx]++;
+            q.add(c);
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
+            if (q.isEmpty()) {
+                System.out.print(-1 + " ");
             } else {
-                q2.add(data);
+                System.out.print(q.peek() + " ");
             }
-        }
-
-        public static int pop() {
-            if (isEmpty()) {
-                System.out.println("Empty");
-                return -1;
-            }
-            int top = -1;
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    top = q1.remove();
-                    if (q1.isEmpty()) {
-                        break;
-                    }
-                    q2.add(top);
-                }
-            } else {
-                while (!q2.isEmpty()) {
-                    top = q2.remove();
-                    if (q2.isEmpty()) {
-                        break;
-                    }
-                    q1.add(top);
-                }
-            }
-            return top;
-        }
-
-        public static int peek() {
-            if (isEmpty()) {
-                System.out.println("Empty");
-                return -1;
-            }
-            int top = -1;
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    top = q1.remove();
-                    q2.add(top);
-                }
-            } else {
-                while (!q2.isEmpty()) {
-                    top = q2.remove();
-                    q1.add(top);
-                }
-            }
-            return top;
         }
     }
 
     public static void main(String args[]) {
-        Stacks q = new Stacks();
-        System.out.println("stacks:- ");
-        q.push(1);
-        q.push(2);
-        q.push(3);
-        q.push(4);
-        q.push(5);
-
-        while (!q.isEmpty()) {
-            System.out.println(q.pop());
-        }
+        firstNonRepetingChar("aabccdb");
     }
 }
