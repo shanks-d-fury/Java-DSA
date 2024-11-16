f480bd1acc942276cccd30526e4ea5f8109b4e85
shanks-D-fury <shashanks9190@gmail.com>
2024-11-17 02:18:59 +0530

    Greedy : chocalate Problem { of min cost using the cut }

--- GreedyAlgorithm.java
+++ GreedyAlgorithm.java
@@ -3,19 +3,37 @@ import java.util.Arrays;
 
 public class GreedyAlgorithm {
     public static void main(String args[]) {
-        int notes[] = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 };
-        int value = 590;
-        Arrays.sort(notes);
-        int i = notes.length - 1;
-        int note = 0;
-        while (value > 0) {
-            if (notes[i] > value) {
-                i--;
+        int vertical[] = { 2, 1, 3, 1, 4 };
+        int horizontal[] = { 4, 1, 2 };
+        int vi = vertical.length - 1;
+        int hj = horizontal.length - 1;
+        int vpart = 1;
+        int hpart = 1;
+        int totalCost = 0;
+        Arrays.sort(vertical);
+        Arrays.sort(horizontal);
+
+        while (vi >= 0 && hj >= 0) {
+            if (horizontal[hj] >= vertical[vi]) {
+                totalCost += (vpart * horizontal[hj]);
+                hpart++;
+                hj--;
             } else {
-                note++;
-                value -= notes[i];
+                totalCost += (hpart * vertical[vi]);
+                vpart++;
+                vi--;
             }
         }
-        System.out.println("Max notes used : " + note);
+        while (vi >= 0) {
+            totalCost += (hpart * vertical[vi]);
+            vpart++;
+            vi--;
+        }
+        while (hj >= 0) {
+            totalCost += (vpart * horizontal[hj]);
+            hpart++;
+            hj--;
+        }
+        System.out.println("Total Minimum cost of cutting is : " + totalCost);
     }
 }
