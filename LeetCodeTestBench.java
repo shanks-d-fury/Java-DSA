f7d11b7e4ce61aba0883d5ba1f4bf5a1389c81f4
Shashank_S <shashanks9190@gmail.com>
2024-11-20 02:46:14 +0530

    LeetCode : Sprial Matrix 2D array

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,53 +1,54 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 public class LeetCodeTestBench {
 
     public static void main(String[] args) {
-        int testArray[][] = { { 1, 4 }, { 0, 0 } };
+        int testArray[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
         //
         long startTime = System.currentTimeMillis();
         //
-        int[][] ansArray = mergeInterval(testArray);
+        System.out.println(spiralOrder(testArray));
         //
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
         System.out.println("Time taken : " + timeTaken + " ms");
-        for (int[] ary : ansArray) {
-            for (int num : ary) {
-                System.out.print(num + " ");
-            }
-            System.out.print("||");
-        }
     }
 
-    public static int[][] mergeInterval(int[][] intervals) {
-        ArrayList<ArrayList<Integer>> arys = new ArrayList<>();
-        if (intervals.length == 0) {
-            return arys.stream()
-                    .map(row -> row.stream().mapToInt(Integer::intValue).toArray())
-                    .toArray(int[][]::new);
-        }
-        if (intervals.length == 1) {
-            return intervals;
-        }
-        Arrays.sort(intervals, Comparator.comparingDouble(o -> o[0]));
-        int i;
-        int prev[] = intervals[0];
-        for (i = 1; i < intervals.length; i++) {
-            int[] crnt = intervals[i];
-            if (prev[1] >= crnt[0]) {
-                prev[1] = Math.max(crnt[0], prev[1]);
-            } else {
-                arys.add(new ArrayList<>(List.of(prev[0], prev[1])));
-                prev = intervals[i];
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> ary = new ArrayList<>();
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+        while (startRow <= endRow && startCol <= endCol) {
+            // Top
+            for (int j = startCol; j <= endCol; j++) {
+                ary.add(matrix[startRow][j]);
             }
+            // Right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                ary.add(matrix[i][endCol]);
+            }
+            // Bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                ary.add(matrix[endRow][j]);
+            }
+            // Left
+            for (int i = endRow - 1; i >= startRow + 1; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                ary.add(matrix[i][startCol]);
+            }
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
         }
-        arys.add(new ArrayList<>(List.of(prev[0], prev[1])));
-        return arys.stream()
-                .map(row -> row.stream().mapToInt(Integer::intValue).toArray())
-                .toArray(int[][]::new);
+        return ary;
     }
 }
