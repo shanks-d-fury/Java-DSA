4ab90e7ca4f318fda94729509adf4142bc14e921
Shashank_S <shashanks9190@gmail.com>
2024-11-24 00:53:27 +0530

    LeetCode : Three sum {pending}

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,54 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LeetCodeTestBench {
 
     public static void main(String[] args) {
-        int testArray[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int testArray[] = { 0, 0, 0 };
         //
         long startTime = System.currentTimeMillis();
         //
-        rotate(testArray);
+        System.out.println(threeSum(testArray));
         //
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
         System.out.println("Time taken : " + timeTaken + " ms");
     }
 
-    public static void rotate(int[][] matrix) {
-        for (int[] mat : matrix) {
-            for (int num : mat) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         System.out.println();
-        int n = matrix.length - 1;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix.length; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        List<List<Integer>> ans = new ArrayList<>();
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int x = nums[i] + nums[j];
+            int k = 0;
+            if (x < 0) {
+                k = j - 1;
+            } else if (x > 0) {
+                k = i + 1;
+            }
+            if (x <= 0) {
+                i++;
+            } else if (x > 0) {
+                j--;
+            }
+            if (x + nums[k] == 0) {
+                ans.add(new ArrayList<>(List.of(nums[i], nums[j], nums[k])));
             }
         }
-        for (int[] mat : matrix) {
-            for (int num : mat) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-        for (int i = 0; i < (matrix.length) / 2; i++) {
-            for (int[] matx : matrix) {
-                int temp = matx[i];
-                matx[i] = matx[n - i];
-                matx[n - i] = temp;
-            }
-        }
-        for (int[] mat : matrix) {
-            for (int num : mat) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
-
+        return ans;
     }
+
 }
