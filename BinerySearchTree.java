fc50b5c5f8c587c3bb519c7bdbb2a7492b09183e
Shashank_S <shashanks9190@gmail.com>
2025-02-12 14:25:47 +0530

    BST : Build BST

---
+++ BinerySearchTree.java
@@ -0,0 +1,46 @@
+
+public class BinerySearchTree {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node buildBST(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (val > root.data) {
+            root.right = buildBST(root.right, val);
+        } else if (val < root.data) {
+            root.left = buildBST(root.left, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < nodes.length; i++) {
+            root = buildBST(root, nodes[i]);
+        }
+
+        inorder(root);
+    }
+}
