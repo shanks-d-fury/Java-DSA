46d75908a803bd125c77b436b6ee3089be8d15f0
Shashank_S <shashanks9190@gmail.com>
2025-03-18 00:01:20 +0530

    Heaps : Maximum element in k'size subarray

--- Heaps.java
+++ Heaps.java
@@ -20,22 +20,27 @@ public class Heaps {
     }
 
     public static void main(String[] args) {
+        // Maximum element in the k'size subarray
         PriorityQueue<Info> pq = new PriorityQueue<>();
-        // int[] ary = Genarate_Random.generateRandomArray(7, -5, 5);
         int[] ary = { -5, -3, 0, -5, 5, -1, 5 };
-        System.out.println(Arrays.toString(ary));
-        pq.add(new Info(ary[0], 0));
-        pq.add(new Info(ary[1], 1));
-        pq.add(new Info(ary[2], 2));
+        // int[] ary = Genarate_Random.generateRandomArray(20, -5, 5);
         int k = 3;
         int[] result = new int[ary.length - k + 1];
+        System.out.println(Arrays.toString(ary));
+
+        for (int i = 0; i < k; i++) {
+            pq.add(new Info(ary[i], i));
+        }
         result[0] = pq.peek().data;
         for (int i = 3, j = 1; i < ary.length; i++, j++) {
-            while (pq.peek().idx <= (i - k))
+            while (!pq.isEmpty() && pq.peek().idx <= (i - k))
                 pq.remove();
             pq.add(new Info(ary[i], i));
             result[j] = pq.peek().data;
         }
-        System.out.println(Arrays.toString(result));
+        String x[] = Arrays.toString(result).replaceAll("[\\[\\]]", "").split(",\\s*");
+        for (String y : x) {
+            System.out.print(y + " ");
+        }
     }
 }
