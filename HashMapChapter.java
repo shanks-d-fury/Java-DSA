a10404a3e14ac22ffa15a92ba7d178e30b85465c
Shashank_S <shashanks9190@gmail.com>
2025-03-18 22:31:29 +0530

    Hashmap : Largest subarray whose sum is equal to K

--- HashMapChapter.java
+++ HashMapChapter.java
@@ -3,20 +3,20 @@ import java.util.HashMap;
 
 public class HashMapChapter {
     public static void main(String[] args) {
-        // Largest subarray length which has the sum zero
+        // Largest subarray length which has the sum equal to k
+        int k = 0;
         HashMap<Integer, Integer> hm = new HashMap<>();
-        int ary[] = Genarate_Random.generateRandomArray(4, -1, 5);
+        int ary[] = Genarate_Random.generateRandomArray(10, -10, 10);
         System.out.println(Arrays.toString(ary));
-        int sum = 0, len = 0;
+        int sum = 0, count = 0;
         for (int j = 0; j < ary.length; j++) {
             sum += ary[j];
-            if (hm.containsKey(sum)) {
-                len = Math.max(j - hm.get(sum), len);
-            } else {
-                hm.put(sum, j);
+            if (hm.containsKey(sum - k)) {
+                count += hm.get(sum - k);
             }
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
         }
-        System.out.println(hm);
-        System.out.println("SubArray with sum 0 => " + len);
+        System.out.println(hm + " k=" + k);
+        System.out.println("SubArray with sum 0 => " + count);
     }
 }
