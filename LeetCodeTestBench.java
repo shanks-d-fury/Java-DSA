492ac636a6052f387ec38157fd35041642adf74b
Shashank_S <shashanks9190@gmail.com>
2025-03-25 02:47:53 +0530

    Leetcode : BALLOON PROBLEM

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -3,48 +3,40 @@ import java.util.*;
 public class LeetCodeTestBench {
 
     public static void main(String[] args) {
-        int testArray[] = Genarate_Random.generateRandomArray(5, -500, 500);
+        // int testArray[] = Genarate_Random.generateRandomArray(5, -500, 500);
         //
         long startTime = System.currentTimeMillis();
         //
-        System.out.println(Arrays.toString(testArray));
-        System.out.println(containsDuplicate(testArray));
+        // System.out.println(Arrays.toString(testArray));
+        System.out.println(maxNumberOfBalloons("loonbalxballpoon"));
         //
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
         System.out.println("Time taken : " + timeTaken + " ms");
     }
 
-    // {Given an integer array nums, return true if any value appears at least twice
-    // in the array, and return false if every element is distinct.}
+    // {Given a string text, you want to use the characters of text to form as many
+    // instances of the word "balloon" as possible.You can use each character in
+    // text at most once. Return the maximum number of instances that can be
+    // formed.}
 
-    public static boolean containsDuplicate(int[] nums) {
-        LinkedHashSet<Integer> hm = new LinkedHashSet<>();
-        for (int x : nums) {
-            if (hm.contains(x)) {
-                return true;
-            } else {
-                hm.add(x);
+    public static int maxNumberOfBalloons(String text) {
+        if (text.length() < "balloon".length()) {
+            return 0;
+        }
+        HashMap<Character, Integer> hm = new HashMap<>();
+        String bln = "balon";
+        for (char x : text.toCharArray())
+            if (bln.contains(String.valueOf(x)))
+                hm.put(x, hm.getOrDefault(x, 0) + 1);
+        int count = Integer.MAX_VALUE;
+        for (char x : bln.toCharArray()) {
+            if (!hm.containsKey(x)) {
+                return 0;
             }
+            count = (x == 'l' || x == 'o') ? Math.min(count, hm.getOrDefault(x, 0) / 2)
+                    : Math.min(count, hm.getOrDefault(x, 0));
         }
-        // HashSet<Integer> set = new HashSet<>();
-        // Add elements to set
-        System.out.println("Using the stream and s foreach method : ");
-        hm.stream().forEach(System.out::println);
-        System.out.println("Using the iterator : ");
-        Iterator<Integer> iterator = hm.iterator();
-        while (iterator.hasNext()) {
-            Integer value = iterator.next();
-            System.out.println(value);
-            // You can also remove elements while iterating
-            // iterator.remove();
-        }
-        System.out.println("Using the forEach : ");
-        // Add elements to set
-        hm.forEach(value -> {
-            System.out.println(value);
-        });
-        return false;
+        return count;
     }
-
 }
