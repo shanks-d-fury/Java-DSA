8ef79187313a6a57961a13d3378b5c33a57c9a7e
Shashank_S <shashanks9190@gmail.com>
2025-03-25 15:06:54 +0530

    LeetCode : isValidSuduko using the Hashset

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -4,27 +4,58 @@ public class LeetCodeTestBench {
 
     public static void main(String[] args) {
         // int testArray[] = Genarate_Random.IntArray(5, -5, 5);
-        int testArray[] = { 2, -5, -5, 2, -4 };
+        // int testArray[] = { 2, -5, -5, 2, -4 };
         //
         long startTime = System.currentTimeMillis();
         //
-        System.out.println(Arrays.toString(twoSum(testArray, -4)));
+        // System.out.println(Arrays.toString(twoSum(testArray, -4)));
         //
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
         System.out.println("Time taken : " + timeTaken + " ms\n");
     }
 
-    public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, List<Integer>> hm = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int comp = target - nums[i];
-            if (hm.containsKey(comp)) {
-                return new int[] { hm.get(comp).get(0), i };
+    public static boolean isValidSudoku(char[][] board) {
+        HashSet<Character> hs = new HashSet<>();
+        // check the row
+        for (int i = 0; i < board.length; i++) {
+            hs.clear();
+            for (int j = 0; j < board[0].length; j++) {
+                char x = board[i][j];
+                if (hs.contains(x))
+                    return false;
+                else if (x != '.')
+                    hs.add(x);
             }
-            hm.putIfAbsent(nums[i], new ArrayList<>());
-            hm.get(nums[i]).add(i);
         }
-        return new int[] { -1, -1 };
+        // check for columns
+        for (int i = 0; i < board.length; i++) {
+            hs.clear();
+            for (int j = 0; j < board[0].length; j++) {
+                char x = board[j][i];
+                if (hs.contains(x))
+                    return false;
+                else if (x != '.')
+                    hs.add(x);
+            }
+        }
+
+        // checking for the 3*3 matrix
+        for (int i = 0; i < 3; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                hs.clear();
+                for (int row = i; row < i + 3; row++) {
+                    for (int col = j; col < j + 3; col++) {
+                        char x = board[row][col];
+                        if (hs.contains(x))
+                            return false;
+                        else if (x != '.')
+                            hs.add(x);
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
