2515db827c930ea1d3417a430ed1c60c794c81f1
Shashank_S <shashanks9190@gmail.com>
2025-03-28 02:07:15 +0530

    Leetcode : Longest consecutive sequence & Reverse LL

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -1,41 +1,44 @@
-import java.util.*;
+import java.util.TreeSet;
 
 public class LeetCodeTestBench {
 
     public static void main(String[] args) {
-        // String testArray[] = Genarate_Random.StringArray(5, 5, false, 0);
-        String testArray[] = { "sha", "ash", "sah", "aas", "asa", "shanks" };
+        int testArray[] = Genarate_Random.IntArray(5, 0, 10);
+        // String testArray[] = { "sha", "ash", "sah", "aas", "asa", "shanks" };
         //
         long startTime = System.currentTimeMillis();
         //
-        System.out.println(groupAnagrams(testArray));
+        System.out.println(longestConsecutive(testArray));
         //
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
         System.out.println("Time taken : " + timeTaken + " ms\n");
     }
 
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> hm = new HashMap<>();
-        for (String str : strs) {
-            String sortString = sortString(str);
-            hm.putIfAbsent(sortString, new ArrayList<>());
-            hm.get(sortString).add(str);
-        }
-        List<List<String>> list = new ArrayList<>();
-        for (Map.Entry<String, List<String>> map : hm.entrySet()) {
-            list.add(map.getValue());
-        }
-        Collections.sort(list, Comparator.comparingInt(List::size));
-        return list;
-    }
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0)
+            return 0;
 
-    public static String sortString(String str) {
-        char[] charAry = str.toCharArray();
-        Arrays.sort(charAry);
-        return new String(charAry);
+        TreeSet<Integer> ts = new TreeSet<>();
+        for (int num : nums) {
+            ts.add(num);
+        }
 
-        // this is the cg method
-        // return new String(str.chars().sorted().toArray(), 0, str.length());
+        int curnt = 1, max = 1;
+        Integer prev = null;
+
+        for (int value : ts) {
+            if (prev != null) {
+                if (value == prev + 1) {
+                    curnt++;
+                    max = Math.max(max, curnt);
+                } else {
+                    curnt = 1;
+                }
+            }
+            prev = value;
+        }
+
+        return max;
     }
 }
---
+++ RemoveDuplicateLL.java
@@ -0,0 +1,18 @@
+/**
+ * Problem Statement:
+ * Given the head of a linked list, remove all duplicates from the unsorted
+ * linked list.
+ * Return the head of the linked list after removing the duplicates.
+ *
+ * Example:
+ * Input: head = [1, 2, 3, 2, 4, 3]
+ * Output: [1, 2, 3, 4]
+ *
+ * Constraints:
+ * - The number of nodes in the linked list is in the range [0, 10^4].
+ * - -10^5 <= Node.val <= 10^5
+ */
+
+// Test case
+// Input: [1, 2, 3, 2, 4, 3]
+// Expected Output: [1, 2, 3, 4]
