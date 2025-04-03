75be2c22089704baa6555535619ccbf4d59df78e
Shashank_S <shashanks9190@gmail.com>
2025-04-03 17:27:47 +0530

    Leetcode : Stream CONVERTING THE double[] to DOUBLESTREAM AND CONVERTING BACK TO STREAM<INTEGER>

--- LeetCodeTestBench.java
+++ LeetCodeTestBench.java
@@ -4,23 +4,22 @@ import java.util.stream.*;
 public class LeetCodeTestBench {
 
     public static void main(String[] args) {
-        // int testArray[] = Genarate_Random.IntArray(5, 0, 10);
-        // String testArray[] = { "sha", "ash", "sah", "aas", "asa", "shanks" };
-        // int ary[] = Genarate_Random.IntArray(10, 0, 100);
         //
         long startTime = System.currentTimeMillis();
         //
+
         System.out.println("Enter a list of numbers : ");
-        int[] list;
+        double[] list;
         try (Scanner scanner = new Scanner(System.in)) {
-            list = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).mapToInt(Integer::intValue)
-                    .toArray();
+            list = Arrays.stream(scanner.nextLine().split(" ")).map(Double::parseDouble)
+                    .mapToDouble(Double::doubleValue).toArray();
         }
-        IntStream steam = Arrays.stream(list);
-        Stream<Integer> steam2 = Arrays.stream(list).boxed();
+        DoubleStream steam = Arrays.stream(list);
+        Stream<Integer> steam2 = Arrays.stream(list).boxed().mapToInt(Double::intValue).boxed();
         steam2.forEach(x -> System.out.print(x + " "));
         System.out.println(steam.average().orElse(-1));
         System.out.println(Arrays.toString(list));
+
         //
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
