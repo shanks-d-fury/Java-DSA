f6d551e0f28109fdfb957202038dc359fc1c55fc
Shashank_S <shashanks9190@gmail.com>
2025-04-05 12:58:04 +0530

    Graphs : cycle detection

--- GraphChapter.java
+++ GraphChapter.java
@@ -15,23 +15,33 @@ public class GraphChapter {
 
     static boolean[] visited = new boolean[6];
 
-    public static void DFS(ArrayList<Edge>[] graph, int crnt) {
-        System.out.print(crnt + " ");
-        visited[crnt] = true;
-        for (Edge edge : graph[crnt]) {
-            if (!visited[edge.dest]) {
-                DFS(graph, edge.dest);
+    public static boolean hasCycle(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = false;
+        }
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i]) { // return hasCycleUtil(graph, i, -1); should not write like this , if one
+                               // component doesn't may be other compenent may have the cycle
+                if (hasCycleUtil(graph, i, -1)) {
+                    return true;
+                }
             }
         }
+        return false;
     }
 
-    public static boolean hasPath(ArrayList<Edge>[] graph, int curt, int dest, boolean[] visited) {
-        if (curt == dest) {
-            return true;
-        }
+    public static boolean hasCycleUtil(ArrayList<Edge>[] graph, int curt, int par) {
         visited[curt] = true;
         for (Edge edge : graph[curt]) {
-            if (!visited[edge.dest] && hasPath(graph, edge.dest, dest, visited)) {
+            if (!visited[edge.dest]) {// here the hascycleutil is not called within the if statement because !visited
+                                      // is true and if we call the hascycleutil then , if no cycle is present then
+                                      // return false or if present return true , in both case the visited will become
+                                      // true and again that checks for the else if statement so we have to write it
+                                      // in a inner "if"
+                if (hasCycleUtil(graph, edge.dest, curt)) {
+                    return true;
+                }
+            } else if (visited[edge.dest] && edge.dest != par) {
                 return true;
             }
         }
@@ -56,20 +66,19 @@ public class GraphChapter {
         graph[2].add(new Edge(2, 4, 5));
         // 3 node
         graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 4, 4));
-        graph[3].add(new Edge(3, 5, 1));
+        // graph[3].add(new Edge(3, 4, 4));
+        // graph[3].add(new Edge(3, 5, 1));
         // 4 node
         graph[4].add(new Edge(4, 2, 5));
-        graph[4].add(new Edge(4, 3, 4));
+        // graph[4].add(new Edge(4, 3, 4));
         graph[4].add(new Edge(4, 5, 3));
         // 5 node
-        graph[5].add(new Edge(5, 3, 1));
+        // graph[5].add(new Edge(5, 3, 1));
         graph[5].add(new Edge(5, 4, 3));
 
         // BSF traversal
         System.out.println("BSF traversal :");
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[graph.length];
         q.add(0);
         while (!q.isEmpty()) {
             int curnt = q.remove();
@@ -84,10 +93,13 @@ public class GraphChapter {
             }
         }
 
-        System.out.println("\nDFS traversal :");
-        DFS(graph, 0);
+        // System.out.println("\nDFS traversal :");
+        // DFS(graph, 0);
 
-        System.out.println("\n\nHAS PATH from 0 to 5: " + hasPath(graph, 0, 5, new boolean[6]));
+        // System.out.println("\n\nHAS PATH from 0 to 5: " + hasPath(graph, 0, 5, new
+        // boolean[6]));
+
+        System.out.println("\nHas cycle for the current graph : " + hasCycle(graph));
 
     }
 }
