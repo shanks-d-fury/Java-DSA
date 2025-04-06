f87656b33474be4c4220e3ad4bfd4a048843fd4a
Shashank_S <shashanks9190@gmail.com>
2025-04-07 00:33:18 +0530

    Graphs : Bipartate Graphs {with coloring method}

--- GraphChapter.java
+++ GraphChapter.java
@@ -13,39 +13,34 @@ public class GraphChapter {
         }
     }
 
-    static boolean[] visited = new boolean[6];
-
-    public static boolean hasCycle(ArrayList<Edge>[] graph) {
-        for (int i = 0; i < visited.length; i++) {
-            visited[i] = false;
+    public static boolean isBiparte(ArrayList<Edge>[] graph) {
+        int color[] = new int[graph.length];
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1;
         }
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < graph.length; i++) {
-            if (!visited[i]) { // return hasCycleUtil(graph, i, -1); should not write like this , if one
-                               // component doesn't may be other compenent may have the cycle
-                if (hasCycleUtil(graph, i, -1)) {
-                    return true;
+            if (color[i] == -1) {
+                q.add(i);
+                color[i] = 0;
+                while (!q.isEmpty()) {
+                    int crntQ = q.remove();
+                    if (color[crntQ] == -1) {
+                        color[crntQ] = 0;
+                    }
+                    for (Edge edge : graph[crntQ]) {
+                        if (color[edge.dest] == -1) {
+                            color[edge.dest] = color[crntQ] == 0 ? 1 : 0;
+                            q.add(edge.dest);
+                        } else if (color[edge.dest] == color[crntQ]) {
+                            return false;
+                        }
+                    }
                 }
             }
-        }
-        return false;
-    }
 
-    public static boolean hasCycleUtil(ArrayList<Edge>[] graph, int curt, int par) {
-        visited[curt] = true;
-        for (Edge edge : graph[curt]) {
-            if (!visited[edge.dest]) {// here the hascycleutil is not called within the if statement because !visited
-                                      // is true and if we call the hascycleutil then , if no cycle is present then
-                                      // return false or if present return true , in both case the visited will become
-                                      // true and again that checks for the else if statement so we have to write it
-                                      // in a inner "if"
-                if (hasCycleUtil(graph, edge.dest, curt)) {
-                    return true;
-                }
-            } else if (visited[edge.dest] && edge.dest != par) {
-                return true;
-            }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
@@ -66,40 +61,19 @@ public class GraphChapter {
         graph[2].add(new Edge(2, 4, 5));
         // 3 node
         graph[3].add(new Edge(3, 1, 3));
-        // graph[3].add(new Edge(3, 4, 4));
-        // graph[3].add(new Edge(3, 5, 1));
+        graph[3].add(new Edge(3, 4, 4));
+        graph[3].add(new Edge(3, 5, 1));
         // 4 node
         graph[4].add(new Edge(4, 2, 5));
-        // graph[4].add(new Edge(4, 3, 4));
+        graph[4].add(new Edge(4, 3, 4));
         graph[4].add(new Edge(4, 5, 3));
         // 5 node
-        // graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 3, 1));
         graph[5].add(new Edge(5, 4, 3));
 
-        // BSF traversal
-        System.out.println("BSF traversal :");
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        while (!q.isEmpty()) {
-            int curnt = q.remove();
-            if (!visited[curnt]) {
-                System.out.print(curnt + " ");
-                visited[curnt] = true;
-                for (Edge edge : graph[curnt]) {
-                    if (!visited[edge.dest]) {
-                        q.add(edge.dest);
-                    }
-                }
-            }
-        }
-
-        // System.out.println("\nDFS traversal :");
-        // DFS(graph, 0);
-
-        // System.out.println("\n\nHAS PATH from 0 to 5: " + hasPath(graph, 0, 5, new
-        // boolean[6]));
-
-        System.out.println("\nHas cycle for the current graph : " + hasCycle(graph));
+        // Biparte graph -> acyclic and cyclic with even number of nodes
+        // Non - Biparte graph -> cyclic with odd number of nodes
+        System.out.println("\nHas cycle for the current graph : " + isBiparte(graph));
 
     }
 }
