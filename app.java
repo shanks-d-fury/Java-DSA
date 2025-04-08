ba28fe21e01b49b21ae3bada5199918ef168974d
Shashank_S <shashanks9190@gmail.com>
2025-04-08 13:54:20 +0530

    Pattern MOST ADVANCED PATTERN

--- app.java
+++ app.java
@@ -1,15 +1,22 @@
 
 import java.util.Scanner;
+
 public class app {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("hello shanks");
-        try (Scanner sc = new Scanner (System.in)) {
+        try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter n: ");
             int n = sc.nextInt();
-            for (int i =0;i<=n;i++){
-                for(int j=0;j<i;j++){
-                    System.out.print("*");
+            int v = n, c = n - 2;
+            System.out.println(1);
+            for (int i = 1; i < n; i++) {
+                System.out.print(v + " ");
+                int x = 0;
+                for (int j = n - 1; j > n - i - 1; j--) {
+                    x += j;
+                    System.out.print(v - x + " ");
                 }
+                v = v + c--;
                 System.out.println();
             }
         }
