362bb46341ad1be8ca18c9d7330d0466109a1bdf
Shashank_S <shashanks9190@gmail.com>
2025-04-09 00:22:30 +0530

    Graphs : BELLMAN_FORD ALGORITHM for directed and negitive weight graph
    NOTE :- for negative weight cycle the bellman ford doesn't work properly

--- GraphChapter.java
+++ GraphChapter.java
@@ -109,44 +109,67 @@ public class GraphChapter {
         }
     }
 
+    public static void BellmanFordAlgo(ArrayList<Edge>[] graph, int src) {
+        int[] dist = new int[graph.length];
+        for (int i = 0; i < dist.length; i++) {
+            if (src != i) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < graph.length - 1; i++) {
+            for (ArrayList<Edge> list : graph) {
+                for (Edge edge : list) {
+                    if (dist[edge.source] != Integer.MAX_VALUE && dist[edge.source] + edge.weight < dist[edge.dest]) {
+                        dist[edge.dest] = dist[edge.source] + edge.weight;
+                    }
+                }
+                System.out.println(Arrays.toString(dist));
+            }
+        }
+        System.out.println();
+        int i = 0;
+        for (int dis : dist) {
+            System.out.println(src + "->" + i + "(" + (dis == Integer.MAX_VALUE ? null : dis) + ")");
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
         @SuppressWarnings("unchecked")
-        ArrayList<Edge>[] graph = new ArrayList[6];
+        ArrayList<Edge>[] graph = new ArrayList[5];
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
 
         // Undirected graphs
-        // 0 node
-        graph[0].add(new Edge(0, 1, 1));
-        graph[0].add(new Edge(0, 2, 2));
-        // 1 node
-        graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 3, 3));
-        // 2 node
-        graph[2].add(new Edge(2, 0, 2));
-        graph[2].add(new Edge(2, 4, 5));
-        // 3 node
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 4, 4));
-        graph[3].add(new Edge(3, 5, 1));
-        // 4 node
-        graph[4].add(new Edge(4, 2, 5));
-        graph[4].add(new Edge(4, 3, 4));
-        graph[4].add(new Edge(4, 5, 3));
-        // 5 node
-        graph[5].add(new Edge(5, 3, 1));
-        graph[5].add(new Edge(5, 4, 3));
+        // // 0 node
+        // graph[0].add(new Edge(0, 1, 1));
+        // graph[0].add(new Edge(0, 2, 2));
+        // // 1 node
+        // graph[1].add(new Edge(1, 0, 1));
+        // graph[1].add(new Edge(1, 3, 3));
+        // // 2 node
+        // graph[2].add(new Edge(2, 0, 2));
+        // graph[2].add(new Edge(2, 4, 5));
+        // // 3 node
+        // graph[3].add(new Edge(3, 1, 3));
+        // graph[3].add(new Edge(3, 4, 4));
+        // graph[3].add(new Edge(3, 5, 1));
+        // // 4 node
+        // graph[4].add(new Edge(4, 2, 5));
+        // graph[4].add(new Edge(4, 3, 4));
+        // graph[4].add(new Edge(4, 5, 3));
+        // // 5 node
+        // graph[5].add(new Edge(5, 3, 1));
+        // graph[5].add(new Edge(5, 4, 3));
 
-        // // Directed Graphs
-        // graph[5].add(new Edge(5, 0));
-        // graph[5].add(new Edge(5, 2));
-        // // graph[4].add(new Edge(4, 0));
-        // graph[4].add(new Edge(4, 1));
-        // graph[2].add(new Edge(2, 3));
-        // graph[3].add(new Edge(3, 1));
-        // graph[0].add(new Edge(0, 3));
-        // graph[0].add(new Edge(0, 4));
+        // Directed Graphs
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+        graph[1].add(new Edge(1, 2, -4));
+        graph[3].add(new Edge(3, 2, 2));
+        graph[3].add(new Edge(3, 4, 4));
+        graph[4].add(new Edge(4, 1, -1));
 
         // Biparte graph -> acyclic and cyclic with even number of nodes
         // Non - Biparte graph -> cyclic with odd number of nodes
@@ -155,10 +178,12 @@ public class GraphChapter {
         // The time complexity for the findpath is O(v^v) which is a exponential
         // for the worst case there will be v vertesis from the curnt vertex
         // findPath(graph, 5, 1, "");
-        for (int i = 0; i < graph.length; i++) {
-            System.out.println("From the source " + i);
-            DijkstraAlgo(graph, i);
-            System.out.println();
-        }
+        // for (int i = 0; i < graph.length; i++) {
+        // System.out.println("From the source " + i);
+        // DijkstraAlgo(graph, i);
+        // System.out.println();
+        // }
+
+        BellmanFordAlgo(graph, 4);
     }
 }
