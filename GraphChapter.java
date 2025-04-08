b285536e60b5d01567b377dccadab45156a5725a
Shashank_S <shashanks9190@gmail.com>
2025-04-08 13:54:54 +0530

    Graphs : Dijkstra's Algorithm

--- GraphChapter.java
+++ GraphChapter.java
@@ -1,24 +1,36 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class GraphChapter {
     public static class Edge {
         int source, dest;
-        // int weight;
+        int weight;
 
         // undirected Graph
-        // public Edge(int source, int dest, int weight) {
-        // this.source = source;
-        // this.dest = dest;
-        // this.weight = weight;
-        // }
-
-        // Directed Graph
-        public Edge(int source, int dest) {
+        public Edge(int source, int dest, int weight) {
             this.source = source;
             this.dest = dest;
+            this.weight = weight;
+        }
+
+        // // Directed Graph
+        // public Edge(int source, int dest) {
+        // this.source = source;
+        // this.dest = dest;
+        // }
+    }
+
+    public static class Pair {
+        int node;
+        int path;
+
+        public Pair(int node, int path) {
+            this.node = node;
+            this.path = path;
         }
     }
 
@@ -67,6 +79,36 @@ public class GraphChapter {
         }
     }
 
+    public static void DijkstraAlgo(ArrayList<Edge>[] graph, int src) {
+        int[] dist = new int[graph.length];
+        boolean[] visited = new boolean[graph.length];
+        for (int i = 0; i < dist.length; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.path));
+        pq.add(new Pair(src, 0));
+        while (!pq.isEmpty()) {
+            Pair cur = pq.remove();
+            if (!visited[cur.node]) {
+                visited[cur.node] = true;
+                for (Edge edge : graph[cur.node]) {
+                    if (dist[edge.source] + edge.weight < dist[edge.dest]) {
+                        dist[edge.dest] = dist[edge.source] + edge.weight;
+                        pq.add(new Pair(edge.dest, dist[edge.dest]));
+                    }
+                }
+            }
+        }
+        int i = 0;
+        for (int dis : dist) {
+            System.out.println(src + "->" + i + "(" + dis + ")");
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
         @SuppressWarnings("unchecked")
         ArrayList<Edge>[] graph = new ArrayList[6];
@@ -76,35 +118,35 @@ public class GraphChapter {
 
         // Undirected graphs
         // 0 node
-        // graph[0].add(new Edge(0, 1, 1));
-        // graph[0].add(new Edge(0, 2, 2));
-        // // 1 node
-        // graph[1].add(new Edge(1, 0, 1));
-        // graph[1].add(new Edge(1, 3, 3));
-        // // 2 node
-        // graph[2].add(new Edge(2, 0, 2));
-        // graph[2].add(new Edge(2, 4, 5));
-        // // 3 node
-        // graph[3].add(new Edge(3, 1, 3));
-        // graph[3].add(new Edge(3, 4, 4));
-        // graph[3].add(new Edge(3, 5, 1));
-        // // 4 node
-        // graph[4].add(new Edge(4, 2, 5));
-        // graph[4].add(new Edge(4, 3, 4));
-        // graph[4].add(new Edge(4, 5, 3));
-        // // 5 node
-        // graph[5].add(new Edge(5, 3, 1));
-        // graph[5].add(new Edge(5, 4, 3));
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 2));
+        // 1 node
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 3));
+        // 2 node
+        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 4, 5));
+        // 3 node
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 4, 4));
+        graph[3].add(new Edge(3, 5, 1));
+        // 4 node
+        graph[4].add(new Edge(4, 2, 5));
+        graph[4].add(new Edge(4, 3, 4));
+        graph[4].add(new Edge(4, 5, 3));
+        // 5 node
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 3));
 
-        // Directed Graphs
-        graph[5].add(new Edge(5, 0));
-        graph[5].add(new Edge(5, 2));
-        // graph[4].add(new Edge(4, 0));
-        graph[4].add(new Edge(4, 1));
-        graph[2].add(new Edge(2, 3));
-        graph[3].add(new Edge(3, 1));
-        graph[0].add(new Edge(0, 3));
-        graph[0].add(new Edge(0, 4));
+        // // Directed Graphs
+        // graph[5].add(new Edge(5, 0));
+        // graph[5].add(new Edge(5, 2));
+        // // graph[4].add(new Edge(4, 0));
+        // graph[4].add(new Edge(4, 1));
+        // graph[2].add(new Edge(2, 3));
+        // graph[3].add(new Edge(3, 1));
+        // graph[0].add(new Edge(0, 3));
+        // graph[0].add(new Edge(0, 4));
 
         // Biparte graph -> acyclic and cyclic with even number of nodes
         // Non - Biparte graph -> cyclic with odd number of nodes
@@ -112,6 +154,11 @@ public class GraphChapter {
 
         // The time complexity for the findpath is O(v^v) which is a exponential
         // for the worst case there will be v vertesis from the curnt vertex
-        findPath(graph, 5, 1, "");
+        // findPath(graph, 5, 1, "");
+        for (int i = 0; i < graph.length; i++) {
+            System.out.println("From the source " + i);
+            DijkstraAlgo(graph, i);
+            System.out.println();
+        }
     }
 }
