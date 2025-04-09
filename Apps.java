b2b79cefff5bfdbd57c5454e829f27522a20a7ce
Shashank_S <shashanks9190@gmail.com>
2025-04-10 02:18:43 +0530

    Apps : old app file name changed

---
+++ Apps.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class Apps {
+    public static int[] findIndexes(int input[], int x, int index, ArrayList<Integer> ary) {
+        if (index == input.length) {
+            return ary.stream().mapToInt(Integer::intValue).toArray();
+        }
+        if (x == input[index]) {
+            ary.add(index);
+        }
+        return findIndexes(input, x, index + 1, ary);
+    }
+
+    public static void main(String[] args) {
+        int[] input = Genarate_Random.IntArray(1, 0, 1);
+        int[] ans = findIndexes(input, 1, 0, new ArrayList<>());
+        System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.stream(ans).max().orElse(-1));
+
+    }
+}
