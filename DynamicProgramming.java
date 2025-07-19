95b3010ce04b7182155d81c5c089efe381ae594d
Shashank_S <shashanks9190@gmail.com>
2025-07-19 12:30:57 +0530

    DP : CLIMBING STRAIS normal recursion, memo, tabula

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
 
 public class DynamicProgramming {
     public static void main(String args[]) {
         long startTime = System.currentTimeMillis();
         //
-        int n = 30;
-        // System.out.println(fabnormal(n));
-        System.out.println(fabdp(n, new int[n + 1]));
+        int n = 3;
+        System.out.println(climbingStaris(n));
+        // int[] ways = new int[n + 1];
+        // Arrays.fill(ways, -1);
+        // System.out.println(climbingStarisDP(n, ways));
+        // System.out.println(climbingStarisDPtab(n));
         //
         long endTime = System.currentTimeMillis();
         System.out.println("Time taken : " + (endTime - startTime) + " ms\n");
@@ -27,5 +31,45 @@ public class DynamicProgramming {
         }
         f[n] = fabdp(n - 1, f) + fabdp(n - 2, f);
         return f[n];
+    }
+
+    // recursion for a special case where the person can climb one/two/three strais
+    // at a time
+    public static int climbingStaris(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        return climbingStaris(n - 1) + climbingStaris(n - 2) + climbingStaris(n - 3);
+    }
+
+    // DP
+    public static int climbingStarisDP(int n, int ways[]) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (ways[n] != -1) {
+            return ways[n];
+        }
+        ways[n] = climbingStarisDP(n - 1, ways) + climbingStarisDP(n - 2, ways);
+        return ways[n];
+    }
+
+    public static int climbingStarisDPtab(int n) {
+        int ways[] = new int[n + 1];
+        ways[0] = 1;
+        ways[1] = 1;
+        ways[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+        System.out.println(Arrays.toString(ways));
+        ;
+        return ways[n];
     }
 }
