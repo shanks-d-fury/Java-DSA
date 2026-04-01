eca72be1605d354504a98e949a0f47edc37502cc
Shashank S <159567566+shanks-d-fury@users.noreply.github.com>
2026-04-01 15:41:27 +0530

    Made an intentional error
    Removed erroneous comment from BackTracking.java.

--- BackTracking.java
+++ BackTracking.java
@@ -33,7 +33,7 @@ public class BackTracking {
                 return false;
             }
         }
-
+        this line is an error pavani reddy madam
         // checking in the column
         for (int i = 0; i < 9; i++) {
             if (board[i][col] == digit) {
